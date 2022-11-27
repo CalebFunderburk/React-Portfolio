6a368d0030299eb92bd982612719e9dc8a6c9d95
Caleb <calebfunderburk@icloud.com>
2022-11-26 19:11:52 -0500

    added resume download

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import resume from '../assets/pdf/resume.pdf'
 
 function Resume() {
     return (
-        <section>
-            <p>Resume component</p>
+        <section className='container text-center'>
+            <div className='col-10 mx-auto'>
+                <h4>Download my resume here</h4>
+                <a href={resume} download><button type='button' className='btn btn-secondary my-3'>Download</button></a>
+            </div>
         </section>
     )
 }
