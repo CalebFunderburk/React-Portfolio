b69d64481340ebbae7f67ed3ad23adfd35979838
Caleb <calebfunderburk@icloud.com>
2023-01-16 14:02:47 -0500

    added details popup for projects

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -4,7 +4,8 @@ import ProjectList from './ProjectList'
 function Portfolio() {
     return (
         <section className='col p-5 backsplash rounded-1 shadow-lg'>
-            <h2 className='text-center mb-5'>Portfolio</h2>
+            <h2 className='text-center'>Portfolio</h2>
+            <p className='text1 text-center'>Hover over a project to see a brief overview</p>
             <ProjectList/>
         </section>
     )
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import github from 'bootstrap-icons/icons/github.svg'
 import picme from '../assets/images/picme-screenshot.png'
 import notetaker from '../assets/images/notetaker-screenshot.png'
@@ -14,50 +14,73 @@ const ProjectList = () => {
             title: 'PicMe',
             link: 'https://picme1.herokuapp.com/',
             repo: 'https://github.com/CalebFunderburk/PicMe',
-            image: picme
-        },
-        {
-            title: 'Note Taker',
-            link: 'https://note-taker-design.herokuapp.com/',
-            repo: 'https://github.com/CalebFunderburk/Note-Taker',
-            image: notetaker
-        },
-        {
-            title: 'Weather On The Go',
-            link: 'https://calebfunderburk.github.io/On-The-Go/',
-            repo: 'https://github.com/CalebFunderburk/On-The-Go',
-            image: onthego
-        },
-        {
-            title: 'Passwizard',
-            link: 'https://calebfunderburk.github.io/Passwizard/',
-            repo: 'https://github.com/CalebFunderburk/Passwizard',
-            image: passwizard
+            image: picme,
+            role: 'Lead Developer',
+            desc: 'MVC social media application built for photographers.',
+            tech: 'JavaScript, Node, Express, Handlebars, Sequelize, Bootstrap, Heroku'
         },
         {
             title: "Chunk'd",
             link: 'https://chunkd-aj.herokuapp.com/signup',
             repo: 'https://github.com/PuppetAJ/ReactMC',
-            image: chunkd
+            image: chunkd,
+            role: 'Backend Developer',
+            desc: 'MERN stack Minecraft clone built into a social media platform.',
+            tech: 'React, Express, Node, MongoDB, GraphQL, ThreeJS, JWT, Tailwind, Heroku'
         },
         {
             title: 'Tech Talk',
             link: 'https://tech-talk-0.herokuapp.com/',
             repo: 'https://github.com/CalebFunderburk/Tech-Talk',
-            image: techtalk
+            image: techtalk,
+            role: 'Full Stack Developer',
+            desc: 'MVC forum application built for trends in technology.',
+            tech: 'JavaScript, Node, Express, Handlebars, Sequelize, Bootstrap, Heroku'
+        },
+        {
+            title: 'Weather On The Go',
+            link: 'https://calebfunderburk.github.io/On-The-Go/',
+            repo: 'https://github.com/CalebFunderburk/On-The-Go',
+            image: onthego,
+            role: 'Lead Developer',
+            desc: 'Open Weather API weather application.',
+            tech: 'HTML, CSS, JQuery, MomentJS, Bulma'
+        },
+        {
+            title: 'Note Taker',
+            link: 'https://note-taker-design.herokuapp.com/',
+            repo: 'https://github.com/CalebFunderburk/Note-Taker',
+            image: notetaker,
+            role: 'Full Stack Developer',
+            desc: 'Simple note taking application.',
+            tech: 'JavaScript, Express, Node, Heroku, Bootstrap, Heroku'
+        },
+        {
+            title: 'Passwizard',
+            link: 'https://calebfunderburk.github.io/Passwizard/',
+            repo: 'https://github.com/CalebFunderburk/Passwizard',
+            image: passwizard,
+            role: 'Full Stack Developer',
+            desc: 'Random password generator application.',
+            tech: 'HTML, CSS, JavaScript'
         }
     ]
 
     return (
-        <div className='row'>
+        <div className='row g-0'>
             {projects.map(project => (
-                <div className='col-12 col-lg-6 card cardfix' key={project.title}>
+                <div className='col-12 col-lg-6 card cardfix position-relative' key={project.title}>
                     <img src={project.image} alt="A screenshot of Caleb's project" className='card-img' />
-                    <div className='container card-img-overlay text-light d-flex align-items-start justify-content-end'>
-                        <div className='projectarticle col-lg-4 col-sm-6 col-10 background p-1 rounded-1 text-end'>
+                    <div className='projectimage container card-img-overlay hover'>
+                        <div className='projectarticle col-sm-4 col-6 background position-absolute top-0 end-0 p-1 rounded-1 text-end'>
                             <p className='card-title text1 fs-5 font-weight-bold projecttext'>{project.title}</p>
                             <a href={project.link} target='_blank' rel='noopener noreferrer' className='card-text projecttext'><p>Visit Site</p></a>
                             <a href={project.repo} target='_blank' rel='noopener noreferrer'><img src={github} alt='GitHub icon' className='githublogo'/></a>
+                        </div>
+                        <div className='projectinfo col-8 mx-auto background position-absolute bottom-0 start-50 translate-middle-x p-1 rounded-1 text1 text-center'>
+                            <p>{project.role}</p>
+                            <p>{project.desc}</p>
+                            <p>{project.tech}</p>
                         </div>
                     </div>
                 </div>
