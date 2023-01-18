8373dd93bf2f69473b9acd1822450dee8dba3197
Caleb <calebfunderburk@icloud.com>
2023-01-17 19:10:02 -0500

    final touches before update

--- src/components/About.js
+++ src/components/About.js
@@ -6,27 +6,20 @@ function About() {
         <section className='py-5'>
             <div className='col mx-auto backsplash text-light rounded-1 p-4 shadow-lg'>
                 {/* Need to handle image sizing when viewed on smaller viewports */}
-                <img src={headshot} alt='A head shot of Caleb Funderburk' className='rounded-circle mx-auto d-block py-2 img-fluid' width='15%'/>
+                <img src={headshot} alt='A head shot of Caleb Funderburk' className='rounded-circle mx-auto d-block py-2 img-fluid'/>
                 <h2 className='py-2 text-center'>About me</h2>
-                <p className='text1 text-left py-5 indent p-5 rounded-1 shadow-lg border border-dark'> 
+                <p className='text1 text-left py-5 indent p-3 rounded-1 shadow-lg border border-dark'> 
                     I am a Full Stack Developer with one year of experience. I love working with 
                     code both on the front-end and the back-end. I currently work with a variety
-                    of languages commonly associated with the MERN stack, but would like to expand 
-                    my knowledge into the realm of ASP.NET one of the days. I have worked on many 
+                    of languages commonly associated with the MERN stack. I have worked on many 
                     projects both solo and on a team. I really enjoy with other developers, combining
                     minds to create something you wouldn't normally be able to make alone is very 
                     inspiring. Outside of the 6 month boot camp I attended to expand my knowledge on 
                     Full Stack Web Development, I have spent an additional 6 months refining my skills
                     and staying up to date on recent changes in technologies and expanding my skill set. 
                     In the ever expanding world that is tech, I find it extremely important to have a 
-                    passion for learning and a standard to relevant. After initially getting into code, I 
-                    also branched into UX/UI Design for a spell. After studying for a while, I learned what 
-                    I really enjoyed doing was coding. However, The skills I learned as a UX/UI Designer 
-                    have stuck with me today when tackling front end development projects. Not only the 
-                    ability to create mockups and think about how something might look visually, but the 
-                    ability to sympathize with the user and their story.
-
-                    <p className='pt-5 text-center text1 revertindent'>Thank you and feel free to reach out or download my resume!</p>
+                    passion for learning and a standard to relevant. 
+                    <p className='pt-5 text-center text1 revertindent'>Thank you, feel free to reach out or download my resume!</p>
                 </p>
             </div>
         </section>
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,14 +6,14 @@ import twitter from 'bootstrap-icons/icons/twitter.svg'
 function Footer() {
     return (
         <footer className='text-center py-5'>
-            <ul className="list-unstyled d-flex justify-content-evenly py-1">
-                <li><a href='https://github.com/CalebFunderburk' target='_blank' rel='noopener noreferrer'><img src={github} alt='GitHub icon' width='200%'/></a></li>
-                <li><a href='https://www.linkedin.com/in/caleb-funderburk/' target='_blank' rel='noopener noreferrer'><img src={linkedin} alt='LinkedIn icon' width='200%'/></a></li>
-                <li><a href='https://twitter.com/CaIebTheGrey' target='_blank' rel='noopener noreferrer'><img src={twitter} alt='Twitter icon' width='200%'/></a></li>
-            </ul>
-            <h4 className='pt-5'>Built by Caleb Funderburk</h4>
+            <h4 className='pt-3'>Built by Caleb Funderburk</h4>
             <h5 className='text1'>calebfunderburk@icloud.com</h5>
             <a href='https://github.com/CalebFunderburk/React-Portfolio' target='_blank'><span>View this project on GitHub</span></a>
+            <ul className="list-unstyled d-flex justify-content-evenly pt-3">
+                <li><a href='https://github.com/CalebFunderburk' target='_blank' rel='noopener noreferrer'><img src={github} alt='GitHub icon' width='150%'/></a></li>
+                <li><a href='https://www.linkedin.com/in/caleb-funderburk/' target='_blank' rel='noopener noreferrer'><img src={linkedin} alt='LinkedIn icon' width='150%'/></a></li>
+                <li><a href='https://twitter.com/CaIebTheGrey' target='_blank' rel='noopener noreferrer'><img src={twitter} alt='Twitter icon' width='150%'/></a></li>
+            </ul>
         </footer>
     )
 }
